8ab6155004b61415ee9fef86e5de5ab512e49f66
Prajjwal <prajjwalpawar1@gmail.com>
2023-10-18 13:17:14 +0530

    adding routes

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+app.config[
+    "MONGO_URI"
+] = "mongodb+srv://prajjwalpawar1:DevServer@devserver.oog2xix.mongodb.net/"
+mongo = PyMongo(app)
 
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/register", methods=["POST"])
+def register():
+    return
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    return
 
 
 if __name__ == "__main__":
