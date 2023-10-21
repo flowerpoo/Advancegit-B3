6fc3649732c37f3f32b63c71d7271a4d018a5486
LeftAttention <info.annagaming@gmail.com>
2023-10-21 15:35:14 +0530

    update task details for each user

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from pymongo import MongoClient
-from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import re
+from bson import ObjectId
 
 import os
 from dotenv import load_dotenv
@@ -20,17 +21,19 @@ login_manager.init_app(app)
 client = MongoClient(mongo_uri)
 db = client['UserDB']
 users_collection = db['users']
+tasks_collection = db['tasks']
 
 class User(UserMixin):
-    def __init__(self, user_id):
+    def __init__(self, user_id, is_admin=False):
         self.id = user_id
+        self.is_admin = is_admin
 
 @login_manager.user_loader
 def load_user(user_id):
     u = users_collection.find_one({"user_id": user_id})
     if not u:
         return None
-    return User(u['user_id'])
+    return User(str(u['_id']) , u.get('is_admin', False))
 
 @app.route('/')
 def home():
@@ -49,7 +52,7 @@ def signup():
             return "Password doesn't meet criteria"
 
         if password == confirm_password:
-            user_data = {'name': name, 'email': email, 'user_id': user_id, 'password': password}
+            user_data = {'name': name, 'email': email, 'user_id': user_id, 'password': password, 'is_admin': False}
             users_collection.insert_one(user_data)
             return redirect(url_for('login'))
         else:
@@ -67,9 +70,9 @@ def login():
         if user:
             user_obj = User(user['user_id'])
             login_user(user_obj)
-            return "Logged in successfully"
+            return redirect(url_for('dashboard'))
         else:
-            return "Invalid credentials"
+            return "Invalid credentials", 401
 
     return render_template('login.html')
 
@@ -78,6 +81,71 @@ def login():
 def logout():
     logout_user()
     return 'Logged out successfully!'
+
+
+@app.route('/tasks', methods=['POST'])
+@login_required
+def create_task():
+
+    if not current_user.is_admin:
+        return "Unauthorized", 401
+    
+    title = request.form['title']
+    description = request.form['description']
+    assigned_user_id = request.form['assigned_user_id']
+
+    task = {'title': title, 'description': description, 'assigned_user_id': assigned_user_id, 'creator_user_id': current_user.id}
+    tasks_collection.insert_one(task)
+
+    return redirect(url_for('dashboard'))
+
+@app.route('/tasks', methods=['GET'])
+@login_required
+def get_tasks():
+
+    if current_user.is_admin:
+        tasks = list(tasks_collection.find({}))
+    else:
+        tasks = list(tasks_collection.find({'assigned_user_id': current_user.id}))
+
+    return jsonify(tasks)
+
+@app.route('/tasks/<task_id>', methods=['PUT'])
+@login_required
+def update_task(task_id):
+
+    if not current_user.is_admin:
+        return "Unauthorized", 401
+
+    updated_data = request.form.to_dict()
+    tasks_collection.update_one({'_id': ObjectId(task_id)}, {'$set': updated_data})
+    return redirect(url_for('dashboard'))
+
+@app.route('/tasks/<task_id>', methods=['DELETE'])
+@login_required
+def delete_task(task_id):
+    if not current_user.is_admin:
+        return "Unauthorized", 401
+
+    tasks_collection.delete_one({'_id': ObjectId(task_id)})
+    return redirect(url_for('dashboard'))
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    all_users = []
+    tasks = []
+
+    current_user_name = users_collection.find_one({"_id": ObjectId(str(current_user.id))})["user_id"]
+    if current_user.is_admin:
+        all_users = list(users_collection.find({}, {'user_id': 1}))
+        tasks = list(tasks_collection.find({}))
+    else:
+        tasks = list(tasks_collection.find({'assigned_user_id': current_user_name}))
+
+    return render_template('dashboard.html', current_user=current_user, all_users=all_users, tasks=tasks)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
