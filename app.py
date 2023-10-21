8ad2c92c4d9fb5186bf2137d5d4d6a71ff70e113
Prajjwal <prajjwalpawar1@gmail.com>
2023-10-21 13:10:21 +0530

    minor fix

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 app.config[
     "MONGO_URI"
-] = "mongodb+srv://prajjwalpawar1:DevServer@devserver.oog2xix.mongodb.net/"
+] = ""
 mongo = PyMongo(app)
 
 
